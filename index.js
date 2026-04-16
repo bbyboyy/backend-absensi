7a9273293c5f2b6c7dd07736b159a60b37d8c4f9
MartinusSuryadi <martinus.suryadi@micropiranti.com>
2026-04-16 13:07:40 +0700

    fix: support render port

--- index.js
+++ index.js
@@ -69,6 +69,8 @@ app.delete("/delete-user/:id", async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server Running on port", PORT);
 });
